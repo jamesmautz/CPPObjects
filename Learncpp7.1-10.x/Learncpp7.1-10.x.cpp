f02e1026598e284f257bdcd2c67d71dac6b08903
JAMES MAUTZ <u1243111@umail.utah.edu>
2025-09-04 14:41:25 -0400

    Actually added new notes.

---
+++ Learncpp7.1-10.x/Learncpp7.1-10.x.cpp
@@ -0,0 +1,29 @@
+// Learncpp7.1-10.x.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// This file and following files will hopefully be better organized than the CPPObjects file and it's related
+// code. This project will contain code used and covered in Learn C++ from lessons 7.1-10.x.
+
+#include <iostream>
+
+
+namespace Addition 
+{
+    int doSomething(int x, int y) 
+    {
+        return x + y;
+    }
+    
+}
+
+namespace Subtraction
+{
+    int doSomething(int x, int y)
+    {
+        return x - y;
+    }
+
+}
+int main()
+{
+    std::cout << Addition::doSomething(5, 5);
+    return 0;
+}
